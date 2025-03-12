ee6f6ec5c0c379193ea3ee34aee2c343c9805f34
kevinlopez <kevin.lopez@imdea.com>
2025-03-12 20:17:29 +0100

    refactoring freshVar method

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,7 +1,7 @@
 module Interpreter where 
 
 import Syntax
-import Data.Set (member, fromList)
+import Data.Set (Set, member, notMember, fromList)
 
 -- M[N/x]
 substitute :: Term -> String -> Term -> Term
@@ -22,14 +22,14 @@ substitute (Case e x m y n) v s =
         (x', m') = 
                 if x == v then (x, m) 
                 else if x `member` (fv s) then
-                    let x' = freshVar x 
+                    let x' = freshVar x (fv s)
                         m' = substitute m x (Var x')
                     in (x', substitute m v s)
                 else (x, substitute m v s)
         (y', n') = 
                 if y == v then (y, n) 
                 else if y `member` (fv s) then
-                    let y' = freshVar y 
+                    let y' = freshVar y (fv s) 
                         n' = substitute n y (Var y')
                     in (y', substitute n v s)
                 else (y, substitute n v s)
@@ -40,7 +40,7 @@ substitute (Lam y m) x n
     | y `member` fv n = Lam newVar (substitute newBody x n)
     | otherwise = Lam y (substitute m x n)
     where 
-        newVar  = freshVar y
+        newVar  = freshVar y (fv n)
         newBody = substitute m y (Var newVar)
 substitute (BinOp op e1 e2) x n = BinOp op (substitute e1 x n) (substitute e2 x n)
 substitute (BinPred pred e1 e2) x n = BinPred pred (substitute e1 x n) (substitute e2 x n)
@@ -49,7 +49,7 @@ substitute (LetVal y e1 e2) x n =
         if y == x then 
             LetVal y e1' e2
         else if y `member` (fv n) then 
-            let y' = freshVar y 
+            let y' = freshVar y (fv n) 
                 e2' = substitute e2 y (Var y')
             in LetVal y' e1' (substitute e2' x n)
         else 
@@ -59,11 +59,11 @@ substitute (Fun f x e) v s =
         Fun f x e 
     else 
         let (f', e') = if f `member` fv s
-                        then let f' = freshVar f 
+                        then let f' = freshVar f (fv s) 
                             in (f', substitute e f (Var f'))
                         else (f, e)
             (x', e'') = if x `member` fv s 
-                        then let x' = freshVar x 
+                        then let x' = freshVar x (fv s) 
                             in (x', substitute e' x (Var x'))
                         else (x, e')
             eFinal = substitute e'' v s  -- Finally, apply substitution
@@ -72,16 +72,18 @@ substitute (Box ctx e) _ _ = Box ctx e
 substitute (LetBox u e1 e2) x m = LetBox u (substitute e1 x m) (substitute e2 x m)
 substitute (Do c) x m = Do (substitute c x m)
 substitute (Ret sigma e) x m = Ret (substituteSubs sigma x m) (substitute e x m)
-substitute (Seq ctx x e c) y n = 
-    let e' = substitute e y n in 
-        if x == y || y `member` (fromList (map fst ctx)) then 
-            Seq ctx x e' c
-        else Seq ctx x e' (substitute c y n)
+-- substitute (Seq ctx x e c) y n = 
+--     let e' = substitute e y n in 
+--         if x == y || y `member` (fromList (map fst ctx)) then 
+--             Seq ctx x e' c
+--         else Seq ctx x e' (substitute c y n)
 substitute (Ann e ty) x n = Ann (substitute e x n) ty
 
 substituteSubs :: Subs -> String -> Term -> Subs 
 substituteSubs [] _ _ = [] 
 substituteSubs ( (y, m) : sigma ) x n  = (y, substitute m x n)  : (substituteSubs sigma x n)
 
-freshVar :: String -> String 
-freshVar x = x ++ "'"
+freshVar :: String -> Set String -> String 
+freshVar x usedVars = head $ filter (`notMember` usedVars) candidates
+    where 
+        candidates = [x ++ show i | i <- [1 :: Int ..]]
---
+++ tests/InterpreterTest.hs
@@ -0,0 +1,18 @@
+import Test.HUnit
+import System.Exit (exitFailure, exitSuccess)
+import Syntax
+import Interpreter 
+
+arg0 = Lam "x" (App (Var "y") (Var "x"))  -- λx. y x
+result0 = substitute arg0 "y" (Var "x")   -- [y → x](λx. y x) 
+expected0 = Lam "x1" (App (Var "x") (Var "x1"))
+test0 = TestCase (assertEqual "0. Testing capture-avoid substitution in lambda" result0 expected0)
+
+tests = TestList [
+                    test0
+                ]
+
+main :: IO ()
+main = do
+    result <- runTestTT tests
+    if failures result > 0 then exitFailure else exitSuccess
