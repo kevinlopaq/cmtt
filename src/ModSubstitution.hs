4a640a034d97cf9c0101cf0a16080adeb912f8e6
kevinlopez <kevin.lopez@imdea.com>
2025-03-25 11:19:13 +0100

    defining modal substitutions for substitutions

--- src/ModSubstitution.hs
+++ src/ModSubstitution.hs
@@ -6,5 +6,17 @@ import Data.List (elemIndex)
 
 
 -- M〚Ψ.N/u〛
-substitute :: Term -> String -> Ctx -> Term -> Term
-substitute (Var x) _ _ _ = Var x
+modSubstitute :: Term -> String -> Ctx -> Term -> Term
+modSubstitute (Var x) _ _ _ = Var x
+modSubstitute FalseT _ _ _ = FalseT
+modSubstitute TrueT _ _ _ = TrueT
+modSubstitute (IntT n) _ _ _ = IntT n
+modSubstitute (Pair e1 e2) u ctx n = Pair (modSubstitute e1 u ctx n) (modSubstitute e2 u ctx n)
+modSubstitute (Fst e) u ctx n = Fst (modSubstitute e u ctx n)
+modSubstitute (Snd e) u ctx n = Snd (modSubstitute e u ctx n)
+modSubstitute (InL e) u ctx n = InL (modSubstitute e u ctx n)
+modSubstitute (InR e) u ctx n = InR (modSubstitute e u ctx n)
+
+modSubstituteSubs :: Subs -> String -> Ctx -> Term -> Subs 
+modSubstituteSubs [] _ _ _ = [] 
+modSubstituteSubs ( (y, m) : sigma ) u ctx n  = (y, modSubstitute m u ctx n)  : (modSubstituteSubs sigma u ctx n)
