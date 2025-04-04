a52a0410957c18fbf5af4115ed9fbe18ecfe6ee7
kevinlopez <kevin.lopez@imdea.com>
2025-04-04 14:54:19 +0200

    finished defining isValue predicate

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -15,8 +15,16 @@ isValue (IntT _) = True
 isValue TrueT = True
 isValue FalseT = True  
 isValue Unit = True
-isValue (Lam _ _) = True 
+isValue (Lam _ _) = True
+isValue (Fun _ _ _) = True
 isValue (Pair e1 e2) = isValue e1 && isValue e2
 isValue (InL e) = isValue e 
 isValue (InR e) = isValue e
-isVale _ = False
+isValue (Box _ _) = True 
+isValue (Do _) = True 
+isValue (Ret sigma e) = isValueSubs sigma && isValue e
+isValue _ = False
+
+isValueSubs :: Subs -> Bool 
+isValueSubs [] = True 
+isValueSubs ((x, e):sigma) = isValue e && isValueSubs sigma 
